466fe581e17923a8439646fe2e17173e8ff8b5f2
Ray Yoon <jaehoonyoon222@gmail.com>
2022-07-09 18:29:09 -0700

    Updating main script

--- logic.js
+++ logic.js
@@ -1,26 +1,4 @@
-// Getting data
-const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson';
-
-d3.json(url).then(function (data) {
-  createFeatures(data.features);
-  //console.log(data.features[0].properties.mag);
-});
-
-function createFeatures(earthquakeData) {
-  // Create a circle for each feature in the features array
-  // The circle sizes should be related to earthquake magnitude
-  function eachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${feature.properties.mag}</p><hr><p>${new Date(feature.properties.time)}</p>`);
-  }
-
-  // Create a GeoJSON layer, insert the features
-  var earthquakes = L.geoJSON(earthquakeData, {
-    eachFeature: eachFeature
-  });
-
-  createMap(earthquakes);
-}
-
+// Function to define map
 function createMap(earthquakes) {
   // Base layers
   var streetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -56,3 +34,64 @@ function createMap(earthquakes) {
   }).addTo(map);
 
 }
+
+// Function to define features
+function createFeatures(response) {
+  // Create a circle for each feature in the features array
+  // The circle sizes should be related to earthquake magnitude
+  ///*
+  var allEarthquakes = response.features;
+  console.log(allEarthquakes);
+
+  /*
+  var eqMarkers = [];
+
+  for (var i = 0; i < allEarthquakes.length; i++) {
+    var quake = allEarthquakes[i];
+
+    var coord = quake.geometry.coordinates;
+    var lat = coord[0];
+    var lng = coord[1];
+
+    var mag = quake.properties.mag;
+
+    eqMarkers.push(
+      L.circle([lat, lng], {
+        radius: mag * 100
+      })
+    );
+  }
+  */
+
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${feature.properties.mag}</p><hr><p>${new Date(feature.properties.time)}</p>`);
+  }
+  
+  /*
+  function eqIntensity () {
+
+  }
+  */
+
+  // Create a GeoJSON layer, insert the features
+  var earthquakes = L.geoJSON(response, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, {
+        radius: 10,
+        color: 'blue',
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      })
+
+    }
+  });
+
+  createMap(earthquakes);
+}
+
+// Getting data
+const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson';
+
+d3.json(url).then(createFeatures);
