7ec0187632a1f61147952e2218d6934c441db643
Ray Yoon <jaehoonyoon222@gmail.com>
2022-07-15 18:14:27 -0700

    Updating files

--- README.md
+++ README.md
@@ -5,9 +5,7 @@ This project uses leaflet to display earthquake GeoJSON data on a map.
 
 ## Credits
 Data sources:
- - USGS GeoJSON Feed, Magnitude 4.5+ Earthquakes Past 30 Days, 7/7/2022
- - World Tectonic Plates and Boundaries, https://github.com/fraxen/tectonicplates
+ - USGS GeoJSON Feed, Magnitude 2.5+ Earthquakes Past 30 Days, 7/15/2022
  
 Map Tiles by:
  - OpenStreetMap
- - MapTiles
--- logic.js
+++ logic.js
@@ -34,54 +34,73 @@ function createMap(earthquakes) {
   }).addTo(map);
 
   // Legend object
-  /*
-  var legend = L.control({position: 'bottomright'});
+  var legend = L.control({position: 'bottomright', collapsed: false});
   legend.onAdd = function(map) {
     var div = L.DomUtil.create('div', 'info-legend'),
-      grades = [],
-      labels = [];
-    for (var i = 0; i < grades.length; i++) {
-      div.innerHTML += '<i style="background:' + '"></i>'
+      labels = ['<strong>Earthquake Depth</strong>'],
+      depths = ['5','5-20','20-40','40+']
+      div.innerHTML = labels.join('<br>')
+    for (var i = 0; i < depths.length; i++) {
+      div.innerHTML += '<i style="background:' + depths[i] +'"></i> ' + depths[i] + '<br>'
     }
+    return div;
   };
   legend.addTo(map);
-  */
 
 }
 
 // Function to define features
 function createFeatures(response) {
+  //console.log(response.features);
 
   // Generates popups at each data point
   function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${feature.properties.mag}</p><hr><p>${new Date(feature.properties.time)}</p>`);
+    layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><p>Magnitude: ${feature.properties.mag}</p><hr><p>Date of Occurrence: ${new Date(feature.properties.time)}</p>`);
   }
   
   // Generates circle markers with properties dependent on earthquake magnitude
   function pointToLayer(feature, latlng) {
 
+    //Defining variables
     var mag = feature.properties.mag;
+    var depth = feature.geometry.coordinates[2];
     var color = '';
     var fillColor = '';
     var radius;
 
-    if (mag >= 5) {
-      color = 'red';
-      fillColor = '#ff2b1c';
-      radius = mag * 1.8;
+    // Conditionals to define size of circle
+    if (mag >= 6) {
+      radius = mag * 2.5;
     }
-    else if (mag < 5 && mag >= 4.6) {
-      color = 'yellow';
-      fillColor = '#ffca1c';
-      radius = mag * 1.4;
+    else if (mag < 6 && mag >= 4.5) {
+      radius = mag * 2;
     }
-    
-    else if (mag < 4.6) {
-      color = 'green';
-      fillColor = '#1ef50f';
+    else if (mag < 4.5 && mag >= 3) {
+      radius = mag * 1.5;
+    }
+    else if (mag < 3) {
       radius = mag;
     }
 
+    // Conditionals to define colors
+    if (depth >= 40) {
+      color = 'red';
+      fillColor = '#ff2b1c';
+    }
+    else if (depth < 40 && depth >= 20) {
+      color = 'orange';
+      fillColor = '#fc8803';
+    }
+    else if (depth < 20 && depth >= 5) {
+      color = 'yellow';
+      fillColor = '#ffca1c';
+    }
+    else if (depth < 5) {
+      color = 'green';
+      fillColor = '#1ef50f';
+    }
+    
+    // Create circle marker at location of earthquake
     return L.circleMarker(latlng, {
       radius: radius,
       color: color,
@@ -104,5 +123,6 @@ function createFeatures(response) {
 }
 
 // Getting data
-const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson';
+const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson';
+
 d3.json(url).then(createFeatures);
